72652737e98277161d5d317e494acf325e2e5b23
AlexKazmirchuk <zedomo@mail.ru>
2016-05-16 12:06:42 +0300

    Added class GameFieldViev will be painted on the playing field

---
+++ java/com/alexkaz/game2048/GameFieldView.java
@@ -0,0 +1,22 @@
+package com.alexkaz.game2048;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.View;
+
+public class GameFieldView extends View {
+
+    private Context context;
+
+    public GameFieldView(Context context) {
+        super(context);
+        this.context = context;
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        //TODO
+    }
+}
