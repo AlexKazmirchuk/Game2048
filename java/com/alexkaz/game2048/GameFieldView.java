1cb78f94e009f4038d015cb50139b015b7c1aad2
AlexKazmirchuk <zedomo@mail.ru>
2016-05-17 11:40:35 +0300

    Added the right code in the CellManager.java

--- java/com/alexkaz/game2048/GameFieldView.java
+++ java/com/alexkaz/game2048/GameFieldView.java
@@ -4,13 +4,31 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.view.View;
 
+import com.alexkaz.game2048.gamelogic.CellManager;
+
+import java.io.File;
+import java.io.IOException;
+
 public class GameFieldView extends View {
 
-    private Context context;
+    private GameActivity context;
+    private CellManager cellManager;
 
-    public GameFieldView(Context context) {
+    public GameFieldView(GameActivity context) {
         super(context);
         this.context = context;
+
+        setOwnParam();
+        initComp();
+    }
+
+    //Методи
+    private void setOwnParam(){
+
+    }
+
+    private void initComp(){
+        cellManager = new CellManager(context);
     }
 
 
@@ -18,5 +36,28 @@ public class GameFieldView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         //TODO
+        if (!getCellManager().lose){
+            cellManager.draw(canvas);
+        }
+        else {
+            System.out.println("Ви програли!!!!!!!!!!!!");
+/*            try {
+                Image lose = ImageIO.read(new File("src/res/YouLose.png"));
+                g.drawImage(lose, 0, 0, null);
+            } catch (IOException ex) {
+                System.out.println("не знайдено");
+            }*/
+        }
+
+
+
+    }
+
+    public CellManager getCellManager() {
+        return cellManager;
+    }
+
+    public void setCellManager(CellManager cellManager) {
+        this.cellManager = cellManager;
     }
 }
