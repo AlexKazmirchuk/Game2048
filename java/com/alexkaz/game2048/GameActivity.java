8ec1c6175c4954da0dc9c943ab4d4b6064108f12
AlexKazmirchuk <zedomo@mail.ru>
2016-05-20 09:00:04 +0300

    Added GestureDetector and method for his initializing

--- java/com/alexkaz/game2048/GameActivity.java
+++ java/com/alexkaz/game2048/GameActivity.java
@@ -2,12 +2,50 @@ package com.alexkaz.game2048;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.widget.Toast;
 
 public class GameActivity extends AppCompatActivity {
+
+    private GestureDetector gestureDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        gestureDetector = initGestureDetector();
     }
+
+    private GestureDetector initGestureDetector() {
+        return new GestureDetector(new GestureDetector.SimpleOnGestureListener() {
+
+            private SwipeDetector detector = new SwipeDetector();
+
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+                                   float velocityY) {
+                try {
+                    if (detector.isSwipeDown(e1, e2, velocityY)) {
+                        showToast("Down Swipe");
+                    } else if (detector.isSwipeUp(e1, e2, velocityY)) {
+                        showToast("Up Swipe");
+                    }else if (detector.isSwipeLeft(e1, e2, velocityX)) {
+                        showToast("Left Swipe");
+                    } else if (detector.isSwipeRight(e1, e2, velocityX)) {
+                        showToast("Right Swipe");
+                    }
+                } catch (Exception e) {} //for now, ignore
+                return false;
+            }
+
+            private void showToast(String phrase){
+                Toast.makeText(getApplicationContext(), phrase, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+
+
 }
