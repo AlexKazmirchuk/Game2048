e102a37e12a31838e709a033af3faac16af78f8d
AlexKazmirchuk <zedomo@mail.ru>
2016-05-25 10:53:54 +0300

    added processing code to musicBtn

--- java/com/alexkaz/game2048/MenuActivity.java
+++ java/com/alexkaz/game2048/MenuActivity.java
@@ -3,13 +3,17 @@ package com.alexkaz.game2048;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
+
+    private GamePreferences gamePreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        gamePreferences = new GamePreferences(this);
     }
 
     public void onClickBtn(View view) {
@@ -24,6 +28,14 @@ public class MenuActivity extends AppCompatActivity {
 
                 break;
             case R.id.musicBtn:
+                if (gamePreferences.getMusicPrefs()){
+                    ((Button)view).setText("Music:off");
+                    gamePreferences.setMusicPrefs(false);
+                }else {
+                    ((Button)view).setText("Music:on");
+                    gamePreferences.setMusicPrefs(true);
+                }
+
 
                 break;
         }
