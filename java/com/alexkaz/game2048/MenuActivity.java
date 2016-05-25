d78f51f0d0dae95c67bd12e67c1736e85ee8a000
AlexKazmirchuk <zedomo@mail.ru>
2016-05-25 10:35:20 +0300

    Added handlers to each button

--- java/com/alexkaz/game2048/MenuActivity.java
+++ java/com/alexkaz/game2048/MenuActivity.java
@@ -2,6 +2,7 @@ package com.alexkaz.game2048;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,5 +10,22 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    public void onClickBtn(View view) {
+        switch (view.getId()){
+            case R.id.resumeBtn:
+
+                break;
+            case R.id.restartBtn:
+
+                break;
+            case R.id.modeBtn:
+
+                break;
+            case R.id.musicBtn:
+
+                break;
+        }
     }
 }
