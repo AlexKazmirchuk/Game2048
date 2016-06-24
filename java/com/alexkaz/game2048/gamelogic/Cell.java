8d080d945ab60e1d5d8ae21b91780e648258d799
AlexKazmirchuk <zedomo@mail.ru>
2016-06-24 10:53:55 +0300

    magic numbers moved into constants

--- java/com/alexkaz/game2048/gamelogic/Cell.java
+++ java/com/alexkaz/game2048/gamelogic/Cell.java
@@ -17,26 +17,50 @@ import com.alexkaz.game2048.R;
 public class Cell {
 
 
+    //for cell
     public static final int SIDE_SPACE_X = 5;
     public static final int SIDE_SPACE_Y = 5;
-
+    public static final int DEFAULT_ID = 0;
     public static final int SHEAR_MAX = 7;
+    public static final int COLOR_OWN = 0;
+    public static final int COLOR_RIGHT = 1;
+    public static final int COLOR_BOTTOM = 2;
+    public static final int DEFAULT_WIDTH = 400;
+    public static final int CELL_SIDE_X_DIVIDER = 88;
+    public static final int CELL_BORDER_X_DIVIDER = 10;
+    public static final int CELL_PIVOT_X_DIVIDER = 10;
+    public static final int CELL_SIDE_Y_DIVIDER = 88;
+    public static final int CELL_BORDER_Y_DIVIDER = 11;
+    public static final int CELL_PIVOT_Y_DIVIDER = 10;
+
+    //for text
+    public static final int START_TEXT_POSITION = 0;
+    public static final String TEXT_SHADOW_COLOR = "#741111";
+    public static final String TEXT_COLOR = "#fa1515";
+    public static final int TEXT_SHEAR = 5;
+    public static final float NORMAL_TEXT_SIZE = 35.0f;
+    public static final float SMALL_TEXT_SIZE = 29.0f;
+    public static final float LARGE_TEXT_SIZE = 55.0f;
+    public static final int THREE_SYMBOLS = 3;
+    public static final int FOUR_SYMBOLS = 4;
+    public static final String RESOURCE_TYPE = "color";
+    public static final int SHEAR_FOR_TEXT = 3; //rename
 
     //Свойства
+    private GameActivity context;
     private int id;
     private int[] colors = new int[3];
-    public int x,y;
-    private GameActivity context;
+
     private boolean locker = true;
-
     private boolean isFresh = false;
-    private int posX, posY;
-    private int shear = 7;
 
+    public int x,y;
+    private int posX, posY;
+
+    private int shear = 7;
     private int shearR = 0;
 
     private int sizeX ;
-
     private int sizeY ;
 
     //Конструктор
@@ -44,7 +68,7 @@ public class Cell {
         this.context = context;
         this.x = x;
         this.y = y;
-        this.id = 0;
+        this.id = DEFAULT_ID;
     }
 
     // Методи
@@ -66,7 +90,7 @@ public class Cell {
 
     private void drawRect(Canvas g){
         Paint p = new Paint();
-        p.setColor(colors[0]);
+        p.setColor(colors[COLOR_OWN]);
         g.drawRect(posX + shearR,posY + shearR,posX+sizeX - SIDE_SPACE_X + shearR,posY+sizeY - SIDE_SPACE_Y + shearR,p);
 
         drawNumbers(g);
@@ -74,7 +98,7 @@ public class Cell {
 
     private void drawRightRomb(Canvas g){
         Paint p = new Paint();
-        p.setColor(colors[1]);
+        p.setColor(colors[COLOR_RIGHT]);
         Path path = new Path();
         path.moveTo(posX + sizeX + shearR - SIDE_SPACE_X, posY + shearR);
         path.lineTo(posX+sizeX+shear - SIDE_SPACE_X, posY+shear);
@@ -86,7 +110,7 @@ public class Cell {
 
     private void drawBottomRomb(Canvas g){
         Paint p = new Paint();
-        p.setColor(colors[2]);
+        p.setColor(colors[COLOR_BOTTOM]);
         Path path = new Path();
         path.moveTo(posX+shearR, posY+sizeY+shearR-SIDE_SPACE_Y);
         path.lineTo(posX+sizeX+shearR- SIDE_SPACE_X, posY+sizeY+shearR-SIDE_SPACE_Y);
@@ -114,43 +138,43 @@ public class Cell {
             p.setTextSize(resizeText(text.length()));
             p.setStyle(Paint.Style.FILL);
             p.setAntiAlias(true);
-            p.getTextBounds(text, 0, text.length(), mTextBoundRect);
+            p.getTextBounds(text, START_TEXT_POSITION, text.length(), mTextBoundRect);
             mTextWidth = p.measureText(text);
             mTextHeight = mTextBoundRect.height();
 
-            p.setColor(Color.parseColor("#741111"));
-            if (shearR <=5){
-                int a = 0;
-                for (int i = 1; i < (5 - shearR); i++) {
+            p.setColor(Color.parseColor(TEXT_SHADOW_COLOR));
+            if (shearR <= TEXT_SHEAR){
+                int paddingFix = 0;
+                for (int i = 1; i < (TEXT_SHEAR - shearR); i++) {
                     g.drawText(text,(centerX - (mTextWidth / 2f)) - SIDE_SPACE_X + i,(centerY + (mTextHeight /2f)) - SIDE_SPACE_Y + i, p);
-                    a = i;
+                    paddingFix = i;
                 }
-                p.setColor(Color.parseColor("#fa1515"));
-                g.drawText(text,(centerX - (mTextWidth/2f)) - SIDE_SPACE_X-a+3,(centerY + (mTextHeight/2f))-SIDE_SPACE_Y-a+3,p);
+                p.setColor(Color.parseColor(TEXT_COLOR));
+                g.drawText(text,(centerX - (mTextWidth/2f)) - SIDE_SPACE_X-paddingFix+ SHEAR_FOR_TEXT,(centerY + (mTextHeight/2f))-SIDE_SPACE_Y-paddingFix+SHEAR_FOR_TEXT,p);
             }
         }
     }
 
     private void setColorFromRes(int id){
-                int i = context.getResources().getIdentifier("colorOwn" + id, "color", context.getPackageName());
-                colors[0] = ContextCompat.getColor(context,i);
+                int i = context.getResources().getIdentifier("colorOwn" + id, RESOURCE_TYPE, context.getPackageName());
+                colors[COLOR_OWN] = ContextCompat.getColor(context,i);
 
-                i = context.getResources().getIdentifier("colorRight" + id, "color", context.getPackageName());
-                colors[1] = ContextCompat.getColor(context,i);
+                i = context.getResources().getIdentifier("colorRight" + id, RESOURCE_TYPE, context.getPackageName());
+                colors[COLOR_RIGHT] = ContextCompat.getColor(context,i);
 
-                i = context.getResources().getIdentifier("colorBottom" + id, "color", context.getPackageName());
-                colors[2] = ContextCompat.getColor(context,i);
+                i = context.getResources().getIdentifier("colorBottom" + id, RESOURCE_TYPE, context.getPackageName());
+                colors[COLOR_BOTTOM] = ContextCompat.getColor(context,i);
     }
 
     private void getSizes(int height, int width) {
 
-        float fxSize = ((float) width /400)*88;
-        float fxBorder = ((float) width /400)*10;
-        float fxPivot = ((float) width /400)*10;
+        float fxSize = ((float) width / DEFAULT_WIDTH)* CELL_SIDE_X_DIVIDER;
+        float fxBorder = ((float) width / DEFAULT_WIDTH)* CELL_BORDER_X_DIVIDER;
+        float fxPivot = ((float) width / DEFAULT_WIDTH)* CELL_PIVOT_X_DIVIDER;
 
-        float fySize = ((float) height /400)*88;
-        float fyBorder = ((float) height /400)*11;
-        float fyPivot = ((float) height /400)*10;
+        float fySize = ((float) height / DEFAULT_WIDTH)* CELL_SIDE_Y_DIVIDER;
+        float fyBorder = ((float) height / DEFAULT_WIDTH)* CELL_BORDER_Y_DIVIDER;
+        float fyPivot = ((float) height / DEFAULT_WIDTH)* CELL_PIVOT_Y_DIVIDER;
 
         sizeX = (int) fxSize;
         int borderX = (int) fxBorder;
@@ -160,21 +184,21 @@ public class Cell {
         int borderY = (int) fyBorder;
         int pivotY = (int) fyPivot;
 
-        this.posX = (sizeX*x) + borderX *x + pivotX;
-        this.posY = (sizeY*y) + borderY *y + pivotY;
+        this.posX = (sizeX*x) + borderX*x + pivotX;
+        this.posY = (sizeY*y) + borderY*y + pivotY;
     }
 
     private float resizeText(int textLength){
-        if (textLength>0){
-            if(textLength == 3){
-                return 35.0f;
-            } else if (textLength == 4){
-                return 29.0f;
+        if (textLength > 0){
+            if(textLength == THREE_SYMBOLS){
+                return NORMAL_TEXT_SIZE;
+            } else if (textLength == FOUR_SYMBOLS){
+                return SMALL_TEXT_SIZE;
             } else {
-                return 55.0f;
+                return LARGE_TEXT_SIZE;
             }
         }
-        return 55.0f;
+        return LARGE_TEXT_SIZE;
     }
 
     public void setId(int id){
