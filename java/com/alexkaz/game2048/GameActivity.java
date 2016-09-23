15ebdb4b9127a5e630a893fe8d71f53b660d5a4e
AlexKazmirchuk <zedomo@mail.ru>
2016-09-23 14:44:19 +0300

    small refactoring

--- java/com/alexkaz/game2048/GameActivity.java
+++ java/com/alexkaz/game2048/GameActivity.java
@@ -15,7 +15,6 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 import com.alexkaz.game2048.controllers.GamePreferences;
 import com.alexkaz.game2048.controllers.SwipeDetector;
 import com.alexkaz.game2048.gamelogic.Direction;
@@ -26,9 +25,13 @@ import java.io.IOException;
 
 public class GameActivity extends AppCompatActivity {
 
+    public static final String GAME_OVER_SOUND = "game_over_sound.mp3";
+    public static final String SWIPE_SOUND = "swipe_sound.mp3";
+    public static final String WIN_SOUND = "win_sound.mp3";
+    public static final String MENU_TAG = "menu";
+
     private GestureDetector gestureDetector;
-    public GamePreferences gamePreferences;
-    private FrameLayout gameFieldWrapper;
+    public  GamePreferences gamePreferences;
     private GameSurfaceView gameSurfaceView;
     private TextView txtScores;
     private TextView txtBestScores;
@@ -38,44 +41,47 @@ public class GameActivity extends AppCompatActivity {
     private AssetManager mAssetManager;
     private int gameOverSound, swipeSound, winSound;
     private int streamID;
-
     private boolean isMusicEnabled = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-
         initComp();
+        initSound();
         readPrefs();
     }
 
     private void initComp(){
         menuDialogFragment = new MenuDialogFragment();
-
         gestureDetector = initGestureDetector();
         gamePreferences = new GamePreferences(this);
-        isMusicEnabled = gamePreferences.getMusicPrefs();
-
-        gameFieldWrapper = (FrameLayout) findViewById(R.id.gameFieldWrapper);
         txtScores = (TextView) findViewById(R.id.txtScores);
         txtBestScores = (TextView) findViewById(R.id.txtBestScores);
-
         gameSurfaceView = new GameSurfaceView(this);
-        gameFieldWrapper.addView(gameSurfaceView, FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT);
+        initGameSurface(gameSurfaceView);
+    }
 
+    private void initGameSurface(GameSurfaceView gameSurfaceView){
+        FrameLayout gameFieldWrapper = (FrameLayout) findViewById(R.id.gameFieldWrapper);
+        gameFieldWrapper.addView(gameSurfaceView, FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT);
         gameSurfaceView.setOnTouchListener(new View.OnTouchListener() {
             public boolean onTouch(View v, MotionEvent event) {
                 return gestureDetector.onTouchEvent(event);
             }
         });
-
         gameSurfaceView.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View arg0) {
-            }
+            public void onClick(View arg0) {}
         });
+    }
 
-        initSound();
+    private void readPrefs() {
+        isMusicEnabled = gamePreferences.getMusicPrefs();
+        int bestScores = gamePreferences.getBestScores();
+        String scoresValue = getString(R.string.txt_scores_default_text) + 0;
+        txtScores.setText(scoresValue);
+        String bestScoresValue = getString(R.string.txt_best_scores_default_text) + bestScores;
+        txtBestScores.setText(bestScoresValue);
     }
 
     private void initSound(){
@@ -85,9 +91,9 @@ public class GameActivity extends AppCompatActivity {
             createNewSoundPool();
         }
         mAssetManager = getAssets();
-        gameOverSound = loadSound("game_over_sound.mp3");
-        swipeSound = loadSound("swipe_sound.mp3");
-        winSound = loadSound("win_sound.mp3");
+        gameOverSound = loadSound(GAME_OVER_SOUND);
+        swipeSound = loadSound(SWIPE_SOUND);
+        winSound = loadSound(WIN_SOUND);
     }
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
@@ -112,8 +118,6 @@ public class GameActivity extends AppCompatActivity {
             afd = mAssetManager.openFd(fileName);
         } catch (IOException e){
             e.printStackTrace();
-            Toast.makeText(getApplicationContext(), "Не могу загрузить файл " + fileName,
-                    Toast.LENGTH_SHORT).show();
             return -1;
         }
         return mSoundPool.load(afd, 1);
@@ -146,23 +150,15 @@ public class GameActivity extends AppCompatActivity {
         super.onPause();
         gamePreferences.saveCellsID(gameSurfaceView.getDrawThreat().getCellManager().getStringCellsID());
         gamePreferences.setGameScores(gameSurfaceView.getDrawThreat().getCellManager().getScores());
-        if (mSoundPool!=null){
+        if (mSoundPool != null){
             mSoundPool.release();
             mSoundPool = null;
         }
     }
 
-    private void readPrefs() {
-        int bestScores = gamePreferences.getBestScores();
-        txtScores.setText("SCORES:0");
-        txtBestScores.setText("BEST SCORES:" + bestScores);
-    }
-
     private GestureDetector initGestureDetector() {
-        return new GestureDetector(new GestureDetector.SimpleOnGestureListener() {
-
+        return new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
             private SwipeDetector detector = new SwipeDetector();
-
             public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
                 try {
                     if (detector.isSwipeDown(e1, e2, velocityY)) {
@@ -178,36 +174,34 @@ public class GameActivity extends AppCompatActivity {
                         playSound(swipeSound);
                         gameSurfaceView.moveCells(Direction.RIGHT);
                     }
-                } catch (Exception e) {} //for now, ignore
+                } catch (Exception ignored) {}
                 return false;
-            }
-
-            private void showToast(String phrase){
-                Toast.makeText(getApplicationContext(), phrase, Toast.LENGTH_SHORT).show();
             }
         });
     }
 
     public void onClickMenuBtn(View view) {
-        menuDialogFragment.show(getFragmentManager(),"menu");
+        menuDialogFragment.show(getFragmentManager(), MENU_TAG);
     }
 
     public void setScores(int points){
-        txtScores.setText("SCORES:" + points);
+        String scoresValue = getString(R.string.txt_scores_default_text) + points;
+        txtScores.setText(scoresValue);
         setBestScores(points);
     }
 
     private void setBestScores(int points){
         int bestScores = gamePreferences.getBestScores();
-
         if(points>bestScores){
-            txtBestScores.setText("BEST SCORES:" + points);
+            String bestScoresValue = getString(R.string.txt_best_scores_default_text) + points;
+            txtBestScores.setText(bestScoresValue);
             gamePreferences.setBestScores(points);
         }
     }
 
     public void setTxtBestScores(int points) {
-        this.txtBestScores.setText("BEST SCORES:" + points);
+        String bestScoresValue = getString(R.string.txt_best_scores_default_text) + points;
+        this.txtBestScores.setText(bestScoresValue);
     }
 
     public  void restartGame(){
@@ -215,18 +209,10 @@ public class GameActivity extends AppCompatActivity {
         setScores(0);
     }
 
-    public boolean isMusicEnabled() {
-        return isMusicEnabled;
-    }
-
-    public void setMusicEnabled(boolean musicEnabled) {
-        isMusicEnabled = musicEnabled;
-    }
-
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_MENU){
-            menuDialogFragment.show(getFragmentManager(),"menu");
+            menuDialogFragment.show(getFragmentManager(),MENU_TAG);
             return true;
         } else {
             return super.onKeyDown(keyCode, event);
@@ -237,5 +223,9 @@ public class GameActivity extends AppCompatActivity {
     protected void onRestart() {
         super.onRestart();
         initSound();
+    }
+
+    public void setMusicEnabled(boolean musicEnabled) {
+        isMusicEnabled = musicEnabled;
     }
 }
